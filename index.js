4e21286d3b6e13d101f9d8d06dc8875d7faa5b71
Aman Khare <khareaman754@gmail.com>
2025-01-09 19:50:45 +0530

    feat: Added google auth apis

--- index.js
+++ index.js
@@ -22,6 +22,7 @@ app.get("/", (req, res) => {
   res.send(`<h1>Welcome to OAuth API Server.</h1>`);
 });
 
+// Github Routes
 app.get("/auth/github", (req, res) => {
   const githubAuthUrl = `https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}&scope=user,repo,security_events`;
 
@@ -31,6 +32,8 @@ app.get("/auth/github", (req, res) => {
 
 app.get("/auth/github/callback", async (req, res) => {
   const { code } = req.query;
+
+  if (!code) return res.status(400).send("Authorization code not provided");
 
   console.log("temporary code: ", code);
   console.log("Github client id: ", process.env.GITHUB_CLIENT_ID);
@@ -60,6 +63,44 @@ app.get("/auth/github/callback", async (req, res) => {
   } catch (error) {
     console.log(error);
     res.status(500).json(error);
+  }
+});
+
+// Google Routes
+app.get("/auth/google", (req, res) => {
+  const googleAuthUrl = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.GOOGLE_CLIENT_ID}&redirect_uri=http://localhost:${PORT}/auth/google/callback&response_type=code&scope=profile email`;
+
+  res.redirect(googleAuthUrl);
+});
+
+app.get("/auth/google/callback", async (req, res) => {
+  const { code } = req.query;
+
+  if (!code) return res.status(400).send("Authorization code not provided");
+
+  let accessToken;
+
+  try {
+    const tokenResponse = await axios.post(
+      `https://oauth2.googleapis.com/token`,
+      {
+        client_id: process.env.GOOGLE_CLIENT_ID,
+        client_secret: process.env.GOOGLE_CLIENT_SECRET,
+        code,
+        grant_type: "authorization_code",
+        redirect_uri: `http://localhost:${PORT}/auth/google/callback`,
+      },
+      {
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      }
+    );
+
+    accessToken = tokenResponse?.data?.access_token;
+    setSecureCookie(res, accessToken);
+    //res.cookie("access_token", accessToken);
+    return res.redirect(`${process.env.FRONTEND_URL}/v1/profile/google`);
+  } catch (error) {
+    console.log(error);
   }
 });
 
